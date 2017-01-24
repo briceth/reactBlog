73bcf52f991276764e5c13ba539b426315afb379
Brice Tessier Huort <tessierhuort@gmail.com>
2017-01-24 16:50:37 +0100

    add style to button add post

--- src/components/postsIndex.js
+++ src/components/postsIndex.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/index';
+ import { Link } from 'react-router';
 
 class PostsIndex extends Component {
-  componentWillMount() {
-    this.props.fetchPosts()
-  }
+  // componentWillMount() {
+  //   this.props.fetchPosts()
+  // }
   render() {
     return (
-      <div></div>
+      <div>
+        <div className='text-xs-right'>
+          <Link to='/post/new' className='pt-button pt-minimal pt-icon-add pt-intent-success'>Add a post</Link>
+        </div>
+        <p>Blog posts!</p>
+      </div>
     )
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,11 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 import PostsIndex from './components/postsIndex';
+import PostNew from './components/postNew';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={PostsIndex}/>
+    <Route path="post/new" component={PostNew} />
   </Route>
 )
