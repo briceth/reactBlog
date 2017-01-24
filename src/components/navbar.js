58ff9ea690a9eb22dddaaccc5473a7aa677bd851
Brice Tessier Huort <tessierhuort@gmail.com>
2017-01-24 10:46:03 +0100

    implement blueprint and navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="pt-navbar .modifier">
+      <div className="pt-navbar-group pt-align-left">
+        <div className="pt-navbar-heading">React blog</div>
+        <input className="pt-input" placeholder="Search files..." type="text" />
+      </div>
+    <div className="pt-navbar-group pt-align-right">
+      <button className="pt-button pt-minimal pt-icon-home">Home</button>
+      <button className="pt-button pt-minimal pt-icon-document">Files</button>
+      <span className="pt-navbar-divider"></span>
+      <button className="pt-button pt-minimal pt-icon-user"></button>
+      <button className="pt-button pt-minimal pt-icon-notifications"></button>
+      <button className="pt-button pt-minimal pt-icon-cog"></button>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
