553da501f4ef2db40bd4497ae72ecdbc4ba28750
Brice Tessier Huort <tessierhuort@gmail.com>
2017-02-01 12:49:46 +0100

    before the crash

--- src/components/formPage.js
+++ src/components/formPage.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import _ from 'lodash';
 import { createPost } from '../actions/index';
 import { connect } from 'react-redux';
 import { reduxForm } from 'redux-form';
